2417ed7542f78101c40811522ee98acfac47e2ac
Anatoly <tolikrut@gmail.com>
2019-06-26 17:58:15 +0300

    Бля изменений рили много. Добавил сортировку Добавил кнопку Refresh Вынес обновление всех данных в отдельный метод

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <el-container>
       <el-main>
+        <el-button @click="refresh">Refresh</el-button>
         <div class="mainContainer">
           <el-row :gutter="40">
             <el-col :span="7">
@@ -36,6 +37,21 @@
               <el-slider range v-model="currentRoomCount" :min="1" :step="1" :max="3"></el-slider>
             </el-col>
           </el-row>
+          <el-row>
+            <el-col :span="4">Сортировать по:</el-col>
+            <el-col :span="4" v-for="sortParam in sortParams" :key="sortParam.value">
+              <span
+                class="sortToogler"
+                @click="sort.desc = sort.type === sortParam.value ? !sort.desc:true; sort.type = sortParam.value;"
+              >
+                {{sortParam.label}}
+                <span
+                  v-if="sort.type === sortParam.value"
+                  :class="`el-icon-arrow-${sort.desc ? 'up' : 'down'}`"
+                ></span>
+              </span>
+            </el-col>
+          </el-row>
           <div class="paddingBox_l" style="display:flex; justify-content: center">
             <el-pagination
               layout="prev, pager, next"
@@ -58,6 +74,10 @@
   max-width: 1300px;
   margin: 0 auto;
 }
+.sortToogler {
+  cursor: pointer;
+  user-select: none;
+}
 </style>
 
 
@@ -70,19 +90,11 @@ export default {
     propertyList,
     appAside
   },
+
   mounted() {
     const coors = this.$el.getBoundingClientRect().bottom
     if (coors < window.innerHeight) {
-      Promise.all([this.getHouses(), this.getPropertys()])
-        .then(([houses, propertys]) => {
-          this.houses.push(...houses.data.data)
-          houses = Array.isArray(propertys.data.data[1])
-            ? [...propertys.data.data]
-            : propertys.data.data
-          this.propertys.push(...houses)
-          this.process = false
-        })
-        .catch(e => alert("Произошла ошибка при загрузке данных"))
+      this.refresh()
       return
     }
     const that = this
@@ -107,13 +119,31 @@ export default {
   },
   data() {
     return {
+      sort: {
+        type: "",
+        desc: false
+      },
       process: false,
       currentRoomCount: [1, 3],
       propertys: [],
       houses: [],
       currentHouse: [],
       currentFloor: [1, 14],
-      page: 1
+      page: 1,
+      sortParams: [
+        {
+          label: "Площадь",
+          value: "area"
+        },
+        {
+          label: "Цена",
+          value: "price"
+        },
+        {
+          label: "Комнатность",
+          value: "rooms_amount"
+        }
+      ]
     }
   },
   computed: {
@@ -121,34 +151,79 @@ export default {
       return this.houses.find(itm => itm.id === this.currentHouse)
     },
     currentShowPropertys() {
-      return this.filteredPropertys.slice((this.page - 1) * 10, this.page * 10)
+      function paramSort(a, b, desc) {
+        if (!desc) {
+          return a > b ? -1 : a < b ? 1 : 0
+        } else {
+          return a < b ? -1 : a > b ? 1 : 0
+        }
+      }
+      return [...this.filteredPropertys]
+        .sort((a, b) => {
+          return paramSort(a[this.sort.type], b[this.sort.type], this.sort.desc)
+        })
+        .slice((this.page - 1) * 10, this.page * 10)
     },
     filteredPropertys() {
-      return this.propertys.filter(itm => {
-        if (
-          (this.currentHouse.length &&
-            this.currentHouse.find(house => house === itm.house_id)) ||
-          !this.currentHouse.length
-        ) {
-          if (
-            this.currentFloor &&
-            itm.floor >= this.currentFloor[0] &&
-            itm.floor <= this.currentFloor[1]
-          ) {
-            if (
-              this.currentRoomCount &&
-              itm.rooms_amount >= this.currentRoomCount[0] &&
-              itm.rooms_amount <= this.currentRoomCount[1]
-            ) {
-              return true
+      return this.propertys
+        .map(
+          ({
+            floor,
+            price: { value: price },
+            area: { area_total: area },
+            rooms_amount,
+            houseName,
+            projectName,
+            preset
+          }) => {
+            return {
+              price,
+              area,
+              rooms_amount,
+              floor,
+              houseName,
+              projectName,
+              preset
             }
           }
-        }
-        return false
-      })
+        )
+        .filter(itm => {
+          if (
+            (this.currentHouse.length &&
+              this.currentHouse.find(house => house === itm.house_id)) ||
+            !this.currentHouse.length
+          ) {
+            if (
+              this.currentFloor &&
+              itm.floor >= this.currentFloor[0] &&
+              itm.floor <= this.currentFloor[1]
+            ) {
+              if (
+                this.currentRoomCount &&
+                itm.rooms_amount >= this.currentRoomCount[0] &&
+                itm.rooms_amount <= this.currentRoomCount[1]
+              ) {
+                return true
+              }
+            }
+          }
+          return false
+        })
     }
   },
   methods: {
+    refresh() {
+      Promise.all([this.getHouses(), this.getPropertys()])
+        .then(([houses, propertys]) => {
+          this.houses.push(...houses.data.data)
+          houses = Array.isArray(propertys.data.data[1])
+            ? [...propertys.data.data]
+            : propertys.data.data
+          this.propertys.push(...houses)
+          this.process = false
+        })
+        .catch(e => alert("Произошла ошибка при загрузке данных"))
+    },
     getPropertys(houseId) {
       let data = new FormData()
       data.append("target", "/property")
@@ -156,7 +231,10 @@ export default {
       return axios({
         method: "POST",
         url: "/api/",
-        data
+        data,
+        maxRedirects: 5,
+        mode: "no-cors"
+        // baseURL: "http://xn---33-6cdtkqbn7a8bm4o.xn--p1ai/"
       })
     },
     getHouses() {
@@ -166,7 +244,10 @@ export default {
       return axios({
         method: "POST",
         url: "/api/",
-        data
+        data,
+        maxRedirects: 5,
+        mode: "no-cors"
+        // baseURL: "http://xn---33-6cdtkqbn7a8bm4o.xn--p1ai/"
       })
     }
   }
