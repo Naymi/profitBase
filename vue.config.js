5b74450283433827803bcfae73b424c4293ebadf
Anatoly <tolikrut@gmail.com>
2019-06-26 17:55:06 +0300

    При билде всё собирается в один файл

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+let cfg = {
   crossorigin: "anonymous",
   devServer: {
     proxy: {
@@ -6,7 +6,32 @@ module.exports = {
         target: "https://жк-династия33.рф",
         ws: true,
         changeOrigin: true
-      },
+      }
+      // disableHostCheck: true,
+      // historyApiFallback: true,
+      // headers: {
+      //   "Access-Control-Allow-Origin": "*",
+      //   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      //   "Access-Control-Allow-Headers":
+      //     "X-Requested-With, content-type, Authorization"
     }
   }
-};
+}
+// console.log("process :", process.env.NODE_ENV)
+if (process.env.NODE_ENV !== "development") {
+  const webpack = require("webpack")
+  cfg = {
+    ...cfg,
+    configureWebpack: {
+      plugins: [
+        new webpack.optimize.LimitChunkCountPlugin({
+          maxChunks: 1
+        })
+      ]
+    },
+    chainWebpack: config => {
+      config.optimization.delete("splitChunks")
+    }
+  }
+}
+module.exports = cfg
